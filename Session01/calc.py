23b089bf2375696966e8559bcd47368aaa51c08d
Christine Lee <clee7@babson.edu>
2018-09-06 10:19:05 -0400

    revision

--- Session01/calc.py
+++ Session01/calc.py
@@ -8,9 +8,9 @@ print(10/1.61)
 # If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace
 # (time per mile in minutes and seconds)? 
 10 / 1.61 # Convert kilometers to miles
-(42 * 60) + 42 # Convert time to seconds
+print((42 * 60) + 42) # Convert time to seconds
 2562/6.211180124223602 # average time (seconds) per mile
-412.482/60 #average time (minutes) per mile
+print(412.482/60) #average time (minutes) per mile
 
 #What is your average speed in miles per hour?
-60/6.874700000000001
+print(60/6.874700000000001)
