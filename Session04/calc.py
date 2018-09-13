249a78a15c1f8043916c089df2d60209e351e11f
Christine Lee <clee7@babson.edu>
2018-09-12 23:34:34 -0400

    work

--- Session04/calc.py
+++ Session04/calc.py
@@ -86,3 +86,27 @@ solution1 = (-b-cmath.sqrt(delta))/(2*a)
 solution2 = (-b+cmath.sqrt(delta))/(2*a)
 
 print('The solutions are {0} and {1}'.format(solution1,solution2))
+
+
+import math
+
+print("Quadratic function : (a * x^2) + b*x + c")
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+r = b**2 - 4*a*c
+
+if r > 0:
+    num_roots = 2
+    x1 = (((-b) + sqrt(r))/(2*a))
+    x2 = (((-b) - sqrt(r))/(2*a))
+    print("Are 2 roots: %f and %f" % (x1, x2))
+elif r == 0: #use an elif statement here, though we haven't technically covered it yet
+    num_roots = 1
+    x = (-b) / 2*a
+    print("There is one root: ", x)
+else:
+    num_roots = 0
+    print("No roots, discriminant < 0.")
+    exit()
